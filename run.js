04d07800f1e47f3f689e93c507a4ed879aa68a4c
leakdev <info@leakoni.net>
2018-03-22 12:14:59 +0100

    Description & Last update
    I made so auth.json is token file due to I were near to spoil my token and it way easyier to work with it like this.

--- run.js
+++ run.js
@@ -6,28 +6,28 @@ client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-// message
+// message (command)
 client.on('message', message => {
 	if (message.content === 'p!ping') {
 		message.channel.send('pong');
 	}
 });
 
-//bot 1
+//bot 1 (command)
 client.on('message', message => {
 	if (message.content === 'p!owner') {
 		message.channel.send('Well, lets see. I think my owners discord Machiko#9793. Feel free to write to him!')
 	} 
 });
 
-//No swearing
+//No swearing (beta testing)
 client.on('message', message => {
-	if (message.content === 'fuck, nigger, fitta') {
+	if (message.content === 'fuck') {
 		message.channel.send('No swearing in this server.')
 	} 
 });
 
-//bot hi
+//bot hi (start on interaction)
 client.on('message', message => {
 	if (message.content === 'hi bot') {
 		message.channel.send('Hi there, I have not much to say right now. But I will feature A.I. later on. So you will be able to have a conversation with me!')
@@ -35,11 +35,11 @@ client.on('message', message => {
 });
 
 
-//welcome
+//welcome (Not working)
 client.on('guildMemberAdd', member => {
 	const channel = member.guild.channels.find('name', 'member-log');
 	if (!channel) return;
-	channel.send(`Welcome to the server, ${member}`);
+	channel.send('Welcome to the server!');
 });
 
 
