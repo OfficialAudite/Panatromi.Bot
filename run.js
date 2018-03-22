29daf19f2019738b82cb33eb24ee6b77ea48e0b8
leakdev <info@leakoni.net>
2018-03-22 14:54:10 +0100

    p!help added

--- run.js
+++ run.js
@@ -4,20 +4,27 @@ const token = require('./auth.json');
 
 client.on('ready', () => {
 	client.user.setStatus('online')
-	client.user.setGame('Pre-Alpha 5/6')
+	client.user.setGame('Pre-Alpha 6/7')
 		.then(console.log)
 		.catch(console.error);
 		console.log('Bot is running!');
 });
 
-// message
+//1.1 help
+client.on('message', message => {
+	if (message.content === 'p!help') {
+		message.channel.send('**Basic** \n`p!owner`, `p!ping`, `p!help`, `hi bot`');
+	}
+});
+	
+//1.2 message
 client.on('message', message => {
 	if (message.content === 'p!ping') {
 		message.channel.send('pong');
 	}
 });
 
-//vote care
+//1.3 vote care
 client.on('message', message => {
 	if (message.content === 'p!vote') {
 		message.react('👍');
@@ -26,21 +33,21 @@ client.on('message', message => {
 	}
 });
 
-//bot 1
+//1.4 bot 1
 client.on('message', message => {
 	if (message.content === 'p!owner') {
 		message.channel.send('Well, lets see. I think my owners discord Machiko#9793. Feel free to write to him!')
 	} 
 });
 
-//No swearing module in progress
+//1.5 No swearing
 client.on('message', message => {
 	if (message.content === 'fuck') {
 		message.channel.send('No swearing in this server.')
 	} 
 });
 
-//bot hi 
+//1.6 bot hi 
 client.on('message', message => {
 	if (message.content === 'hi bot') {
 		message.channel.send('Hi there, I have not much to say right now. But I will feature A.I. later on. So you will be able to have a conversation with me!')
@@ -48,7 +55,7 @@ client.on('message', message => {
 });
 
 
-//welcome (not working)
+//1.7 welcome (not working)
 client.on('guildMemberAdd', member => {
 	const channel = member.guild.channels.find('name', 'member-log');
 	if (!channel) return;
